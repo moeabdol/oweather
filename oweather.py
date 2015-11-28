e66eb9d5b1f7f313a31e05a1b33e3027f2a09697
moeabdol <admin.r99@gmail.com>
2015-11-28 18:53:34 +0300

    clean code

--- oweather.py
+++ oweather.py
@@ -4,66 +4,74 @@ import json
 import ujson
 import argparse
 from argparse import ArgumentParser
-import ipdb
 
 class OpenWeatherMapAPIWrapper:
   def __init__(self, api_key):
     self.api_key = api_key
+    self.current_weather_end_point = \
+      "http://api.openweathermap.org/data/2.5/weather"
+    self.five_day_three_hour_forecast_end_point = \
+      "http://api.openweathermap.org/data/2.5/forecast"
+    self.daily_forecast_end_point = \
+      "http://api.openweathermap.org/data/2.5/forecast/daily"
 
-  def get_current_weather_by_city_name(self, city_name):
-    end_point = "http://api.openweathermap.org/data/2.5/weather"
-    city_query = "?q={}".format(city_name)
+  def get_current_weather_by_city_name(self, city):
+    city_query = "?q={}".format(city)
     appid_query = "&APPID={}".format(self.api_key)
-    response = requests.get(end_point + city_query + appid_query)
-    return ujson.loads(response.text)
+    end_point = self.current_weather_end_point + city_query + appid_query
+    return ujson.loads(requests.get(end_point).text)
 
-  def get_5day_forcast_by_city_name(self, city_name):
-    end_point = "http://api.openweathermap.org/data/2.5/forecast"
-    city_query = "?q={}".format(city_name)
+  def get_five_day_three_hour_forcast_by_city_name(self, city):
+    city_query = "?q={}".format(city)
     appid_query = "&APPID={}".format(self.api_key)
-    response = requests.get(end_point + city_query + appid_query)
-    return ujson.loads(response.text)
+    end_point = self.five_day_three_hour_forecast_end_point + city_query + \
+                appid_query
+    return ujson.loads(requests.get(end_point).text)
 
-  def get_daily_forcast_by_city_name(self, city_name, days=7):
-    end_point = "http://api.openweathermap.org/data/2.5/forecast/daily"
-    city_query = "?q={}".format(city_name)
+  def get_daily_forcast_by_city_name(self, city, days):
+    city_query = "?q={}".format(city)
     days_query = "&cnt={}".format(days)
     appid_query = "&APPID={}".format(self.api_key)
-    response = requests.get(end_point + city_query + days_query +
-                            appid_query)
-    return ujson.loads(response.text)
+    end_point = self.daily_forecast_end_point + city_query + days_query + \
+                appid_query
+    return ujson.loads(requests.get(end_point).text)
 
 def process_arguments():
   parser = ArgumentParser(description="Get weather information from the " +
                                       "Open Weather Map API")
   forecast_group = parser.add_mutually_exclusive_group()
-  forecast_group.add_argument("-f",
-                              "--forecast",
-                              dest="forecast",
-                              action="store_true",
-                              help="Get five day weather forcast for city")
-  forecast_group.add_argument("-d",
-                              "--days",
-                              dest="n",
-                              type=int,
-                              help="Get N days weather forcast for city")
+  forecast_group.add_argument(
+      "-f",
+      "--forecast",
+      dest="forecast",
+      action="store_true",
+      help="Get five day weather forcast for city")
+  forecast_group.add_argument(
+      "-d",
+      "--days",
+      dest="n",
+      type=int,
+      help="Get N days weather forcast for city")
   key_group = parser.add_mutually_exclusive_group(required=True)
-  key_group.add_argument("-k",
-                      "--key",
-                      type=str,
-                      help="Open weather map API key")
-  key_group.add_argument("--key-file",
-                         dest="file",
-                         type=argparse.FileType("r"),
-                         help="Text file with open weather map API key")
-  parser.add_argument("city",
-                      help="Name of city you want to get weather for",
-                      nargs=1)
+  key_group.add_argument(
+      "-k",
+      "--key",
+      type=str,
+      help="Provide open weather map API key from command line")
+  key_group.add_argument(
+      "--key-file",
+      dest="file",
+      type=argparse.FileType("r"),
+      help="Provide open weather map API key file")
+  parser.add_argument(
+      "city",
+      help="Name of city you want to get weather for",
+      nargs=1)
   return parser.parse_args()
 
 def print_weather_info(owmaw, args):
   if args.forecast is True:
-    print_json(owmaw.get_5day_forcast_by_city_name(args.city[0]))
+    print_json(owmaw.get_five_day_three_hour_forcast_by_city_name(args.city[0]))
   elif args.n > 0:
     print_json(owmaw.get_daily_forcast_by_city_name(args.city[0], days=args.n))
   else:
@@ -76,7 +84,7 @@ def print_json(json_dict):
 def get_api_key_from_file(f):
   for line in f.readlines():
     key, value = line.partition("=")[::2]
-    if key.strip() == "openweathermap_api_key":
+    if key.strip() == "oweather_api_key":
       return value.strip()[1:-1]
 
 def get_api_key(args):
