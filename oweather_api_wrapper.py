e49ad20272f90032d6910660ba530077d9a94463
moeabdol <admin.r99@gmail.com>
2015-12-01 03:25:33 +0300

    handle network and invalid api key exceptions

--- oweather_api_wrapper.py
+++ oweather_api_wrapper.py
@@ -1,5 +1,7 @@
+import sys
 import ujson
 import requests
+import ipdb
 
 class OpenWeatherMapAPIWrapper:
     def __init__(self, api_key):
@@ -17,8 +19,13 @@ class OpenWeatherMapAPIWrapper:
         appid_query = "&APPID={}".format(self.api_key)
         end_point = self.current_weather_end_point + city_query + \
                     units_query + appid_query
-        return {"json": ujson.loads(requests.get(end_point).text),
-                "units": units}
+        try:
+            return {"json": ujson.loads(requests.get(end_point).text),
+                    "units": units}
+        except requests.exceptions.ConnectionError:
+            sys.exit("Network Error")
+        except ValueError:
+            sys.exit("Invalid API Key")
 
     def get_five_day_three_hour_forecast_by_city_name(self, city, units):
         city_query = "?q={}".format(city)
@@ -26,8 +33,13 @@ class OpenWeatherMapAPIWrapper:
         appid_query = "&APPID={}".format(self.api_key)
         end_point = self.five_day_three_hour_forecast_end_point + city_query + \
                     units_query + appid_query
-        return {"json": ujson.loads(requests.get(end_point).text),
-                "units": units}
+        try:
+            return {"json": ujson.loads(requests.get(end_point).text),
+                    "units": units}
+        except requests.exceptions.ConnectionError:
+            sys.exit("Network Error")
+        except ValueError:
+            sys.exit("Invalid API Key")
 
     def get_daily_forecast_by_city_name(self, city, days, units):
         city_query = "?q={}".format(city)
@@ -36,6 +48,11 @@ class OpenWeatherMapAPIWrapper:
         appid_query = "&APPID={}".format(self.api_key)
         end_point = self.daily_forecast_end_point + city_query + days_query + \
                     units_query + appid_query
-        return {"json": ujson.loads(requests.get(end_point).text),
-                "days": days,
-                "units": units}
+        try:
+            return {"json": ujson.loads(requests.get(end_point).text),
+                    "days": days,
+                    "units": units}
+        except requests.exceptions.ConnectionError:
+            sys.exit("Network Error")
+        except ValueError:
+            sys.exit("Invalid API Key")
